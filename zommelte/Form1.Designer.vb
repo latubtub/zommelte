66c8c373152a8e61ca9e60ff68e99bf3b347b34b
anatole france <ccproextra@hotmail.com>
2017-09-18 16:19:22 +0200

    fiche inscription

--- zommelte/Form1.Designer.vb
+++ zommelte/Form1.Designer.vb
@@ -22,47 +22,137 @@ Partial Class Form1
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BValider = New System.Windows.Forms.Button()
+        Me.LabelInscription = New System.Windows.Forms.Label()
+        Me.Tnom = New System.Windows.Forms.TextBox()
+        Me.TPrenom = New System.Windows.Forms.TextBox()
+        Me.TadressePostale = New System.Windows.Forms.TextBox()
+        Me.Tville = New System.Windows.Forms.TextBox()
+        Me.Icodepostal = New System.Windows.Forms.TextBox()
+        Me.Temail = New System.Windows.Forms.TextBox()
+        Me.BtAjouterPhoto = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'CheckBox1
+        'BValider
         '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(59, 76)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
-        Me.CheckBox1.TabIndex = 0
-        Me.CheckBox1.Text = "CheckBox1"
-        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.BValider.BackColor = System.Drawing.Color.Blue
+        Me.BValider.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BValider.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BValider.Location = New System.Drawing.Point(374, 259)
+        Me.BValider.Name = "BValider"
+        Me.BValider.Size = New System.Drawing.Size(101, 34)
+        Me.BValider.TabIndex = 1
+        Me.BValider.Text = "Valider"
+        Me.BValider.UseVisualStyleBackColor = False
         '
-        'Button1
+        'LabelInscription
         '
-        Me.Button1.BackColor = System.Drawing.Color.Blue
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Button1.Location = New System.Drawing.Point(59, 136)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(170, 74)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = False
+        Me.LabelInscription.AutoSize = True
+        Me.LabelInscription.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelInscription.Location = New System.Drawing.Point(230, 9)
+        Me.LabelInscription.Name = "LabelInscription"
+        Me.LabelInscription.Size = New System.Drawing.Size(121, 25)
+        Me.LabelInscription.TabIndex = 2
+        Me.LabelInscription.Text = "Inscription"
+        '
+        'Tnom
+        '
+        Me.Tnom.Location = New System.Drawing.Point(235, 67)
+        Me.Tnom.Name = "Tnom"
+        Me.Tnom.Size = New System.Drawing.Size(100, 20)
+        Me.Tnom.TabIndex = 3
+        Me.Tnom.Text = "Nom"
+        '
+        'TPrenom
+        '
+        Me.TPrenom.Location = New System.Drawing.Point(235, 93)
+        Me.TPrenom.Name = "TPrenom"
+        Me.TPrenom.Size = New System.Drawing.Size(100, 20)
+        Me.TPrenom.TabIndex = 4
+        Me.TPrenom.Text = "Prenom"
+        '
+        'TadressePostale
+        '
+        Me.TadressePostale.Location = New System.Drawing.Point(235, 119)
+        Me.TadressePostale.Name = "TadressePostale"
+        Me.TadressePostale.Size = New System.Drawing.Size(240, 20)
+        Me.TadressePostale.TabIndex = 5
+        Me.TadressePostale.Text = "Adresse postale"
+        '
+        'Tville
+        '
+        Me.Tville.Location = New System.Drawing.Point(235, 145)
+        Me.Tville.Name = "Tville"
+        Me.Tville.Size = New System.Drawing.Size(240, 20)
+        Me.Tville.TabIndex = 6
+        Me.Tville.Text = "Ville"
+        '
+        'Icodepostal
+        '
+        Me.Icodepostal.Location = New System.Drawing.Point(235, 171)
+        Me.Icodepostal.Name = "Icodepostal"
+        Me.Icodepostal.Size = New System.Drawing.Size(240, 20)
+        Me.Icodepostal.TabIndex = 7
+        Me.Icodepostal.Text = "Code Postal"
+        '
+        'Temail
+        '
+        Me.Temail.Location = New System.Drawing.Point(235, 197)
+        Me.Temail.Name = "Temail"
+        Me.Temail.Size = New System.Drawing.Size(240, 20)
+        Me.Temail.TabIndex = 8
+        Me.Temail.Text = "email"
+        '
+        'BtAjouterPhoto
+        '
+        Me.BtAjouterPhoto.Location = New System.Drawing.Point(54, 197)
+        Me.BtAjouterPhoto.Name = "BtAjouterPhoto"
+        Me.BtAjouterPhoto.Size = New System.Drawing.Size(127, 23)
+        Me.BtAjouterPhoto.TabIndex = 9
+        Me.BtAjouterPhoto.Text = "Ajouter une photo"
+        Me.BtAjouterPhoto.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(54, 63)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(127, 102)
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Chartreuse
-        Me.ClientSize = New System.Drawing.Size(284, 261)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.CheckBox1)
+        Me.ClientSize = New System.Drawing.Size(514, 323)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.BtAjouterPhoto)
+        Me.Controls.Add(Me.Temail)
+        Me.Controls.Add(Me.Icodepostal)
+        Me.Controls.Add(Me.Tville)
+        Me.Controls.Add(Me.TadressePostale)
+        Me.Controls.Add(Me.TPrenom)
+        Me.Controls.Add(Me.Tnom)
+        Me.Controls.Add(Me.LabelInscription)
+        Me.Controls.Add(Me.BValider)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "s"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents CheckBox1 As CheckBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents BValider As Button
+    Friend WithEvents LabelInscription As Label
+    Friend WithEvents Tnom As TextBox
+    Friend WithEvents TPrenom As TextBox
+    Friend WithEvents TadressePostale As TextBox
+    Friend WithEvents Tville As TextBox
+    Friend WithEvents Icodepostal As TextBox
+    Friend WithEvents Temail As TextBox
+    Friend WithEvents BtAjouterPhoto As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- zommelte/Form1.vb
+++ zommelte/Form1.vb
@@ -1,3 +1,33 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BValider.Click
 
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles LabelInscription.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles Tnom.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs) Handles TPrenom.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged_2(sender As Object, e As EventArgs) Handles TadressePostale.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged_3(sender As Object, e As EventArgs) Handles Tville.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged_4(sender As Object, e As EventArgs) Handles Temail.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
